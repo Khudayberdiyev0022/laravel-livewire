14306835da0a8c1567587e99e1df05be7effd012
khamza <khamza.khudayberdiyev@gmail.com>
2023-07-18 09:25:30 +0500

    updated

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -41,13 +41,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <!-- AdminLTE App -->
 <script src="{{ asset('admin/dist/js/adminlte.min.js') }}"></script>
 <script>
-    /**
-     *
-     * You can write your JS code here, DO NOT touch the default style file
-     * because it will make it harder for you to update.
-     *
-     */
-
     "use strict";
 
     // sidebar active class
